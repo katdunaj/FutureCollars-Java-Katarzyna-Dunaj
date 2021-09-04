1088953551298c77f95cde7b80cf8e21aba2f8f3
katdunaj@gmail.com <katarzyna.ruda112@gmail.com>
2021-09-04 13:46:59 +0200

    Add #idea for gitignore

--- app/src/main/java/pl/futurecollars/invoicing/app/App.java
+++ app/src/main/java/pl/futurecollars/invoicing/app/App.java
@@ -1,18 +1,12 @@
 package pl.futurecollars.invoicing.app;
 
-import static pl.futurecollars.invoicing.app.MessageUtils.getMessage;
-import static pl.futurecollars.invoicing.utilities.StringUtils.join;
-import static pl.futurecollars.invoicing.utilities.StringUtils.split;
-
-import org.apache.commons.text.WordUtils;
-import pl.futurecollars.invoicing.list.LinkedList;
-
 public class App {
 
+    public String getGreeting(){
+        return "Hello World";
+    }
     public static void main(String[] args) {
-        LinkedList tokens;
-        tokens = split(getMessage());
-        String result = join(tokens);
-        System.out.println(WordUtils.capitalize(result));
+        System.out.println(new App(). getGreeting());
+
     }
 }
