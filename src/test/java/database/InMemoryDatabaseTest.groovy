ece2ee240bdba6f7706e284ad3abda568f511ac6
katdunaj@gmail.com <katarzyna.ruda112@gmail.com>
2021-11-23 20:40:21 +0100

    First version test for Rest service

--- src/test/java/database/InMemoryDatabaseTest.groovy
+++ src/test/java/database/InMemoryDatabaseTest.groovy
@@ -1,15 +1,13 @@
-package db
+package database
 
 import pl.futurecollars.invoicing.db.Database
 import pl.futurecollars.invoicing.db.InMemoryDatabase
 
 class InMemoryDatabaseTest extends DatabaseTest {
 
-
     @Override
     Database getDatabaseInstance() {
 
         return new InMemoryDatabase()
-
     }
 }
